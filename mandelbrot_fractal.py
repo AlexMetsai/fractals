5ddceeb8af26d74801b80d9b58e423e20a02bbfe
Alexandros Metsai <alexmetsai@gmail.com>
2021-02-16 18:56:10 +0200

    todos

--- mandelbrot_fractal.py
+++ mandelbrot_fractal.py
@@ -8,3 +8,18 @@ def mandelbrot_fractal(c):
         z = z*z + c
         n += 1
     return n
+
+if __name__ == "__main__":
+    
+    # Set output image resolution.
+    img_width  = 1920
+    img_height = 1080
+    
+    # cast pixel values to complex number
+    # TODO
+    
+    # Assign a color with respect to the number 
+    # of iterations that 'mandelbrot_fractal' returns.
+    # TODO
+    
+    # Save to image
