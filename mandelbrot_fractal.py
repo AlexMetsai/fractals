e6827eeec450cd1b256df97039348176423fcc16
Alexandros Metsai <alexmetsai@gmail.com>
2021-02-27 20:51:13 +0200

    Update mandelbrot_fractal.py

--- mandelbrot_fractal.py
+++ mandelbrot_fractal.py
@@ -15,6 +15,19 @@ if __name__ == "__main__":
     img_width  = 1920
     img_height = 1080
     
+    # plotting window
+    x_leftmost, x_rightmost = -2, 2
+    y_top, y_bottom         = -1, 1
+    
+    # Create image.
+    img  = Image.new("RGB", (img_width, img_height), (0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    
+    # For every pixel, assign a color w.r.t. the number of iterations
+    # returned by the 'mandelbrot_fractal' function.
+    for x in range(img_width):
+        for y in range(img_height):
+    
     # cast pixel values to complex number
     # TODO
     
