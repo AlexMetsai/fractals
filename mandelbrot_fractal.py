8dca9718020bf4f56a5bb8f866c59aa2678ebef9
alexm <alexmetsai@gmail.com>
2021-11-28 23:29:10 +0200

    julia set init commit; minor PEP8 in mandelbrot

--- mandelbrot_fractal.py
+++ mandelbrot_fractal.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright (C) 2021 Alexandros I. Metsai
 alexmetsai@gmail.com
 
@@ -14,33 +14,35 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 
 from PIL import Image, ImageDraw
 
-max_iter = 80
 
-def mandelbrot_fractal(c):
+def mandelbrot_fractal(c, max_iter):
     z, n = 0, 0
-    while abs(z) <=2 and n < max_iter:
-        z = z*z + c
+    while abs(z) <= 2 and n < max_iter:
+        z = z * z + c
         n += 1
     return n
 
+
 if __name__ == "__main__":
-    
+
+    max_iter_ = 80
+
     # Set output image resolution.
-    img_width  = 1920
+    img_width = 1920
     img_height = 1080
-    
+
     # plotting window
     x_leftmost, x_rightmost = -2, 2
-    y_bottom, y_top         = -1, 1
-    
+    y_bottom, y_top = -1, 1
+
     # Create image.
-    img  = Image.new("RGB", (img_width, img_height), (0, 0, 0))
+    img = Image.new("RGB", (img_width, img_height), (0, 0, 0))
     draw = ImageDraw.Draw(img)
-    
+
     # For every pixel:
     #    - convert coordinates to complex number
     #    - assign a color w.r.t. the number of iterations
@@ -48,11 +50,11 @@ if __name__ == "__main__":
     #    - draw color to map
     for x in range(img_width):
         for y in range(img_height):
-            c = complex(x_leftmost + (x/img_width)*(x_rightmost - x_leftmost),
-                        y_bottom + (y/img_height)*(y_top - y_bottom))
-            iters = mandelbrot_fractal(c)
-            color = 255 - int(iters*255/max_iter)
-            draw.point([x,y], (color, color, color))
-    
+            c_ = complex(x_leftmost + (x / img_width) * (x_rightmost - x_leftmost),
+                         y_bottom + (y / img_height) * (y_top - y_bottom))
+            iters = mandelbrot_fractal(c_, max_iter_)
+            color = 255 - int(iters * 255 / max_iter_)
+            draw.point([x, y], (color, color, color))
+
     # Save the generated image.
     img.save("mandelbrot_fractal.png", "PNG")
