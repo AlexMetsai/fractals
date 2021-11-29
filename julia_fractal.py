d390099c9907ea1d7a1acb3e8fe9e7a104043aa2
alexm <alexmetsai@gmail.com>
2021-11-29 19:03:24 +0200

    alternative plotting for julia set

--- julia_fractal.py
+++ julia_fractal.py
@@ -16,20 +16,57 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-max_iter = 100
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def julia_fractal(z, c):
+def julia_set(z, c, max_iter):
     """
     Fractals arising from julia sets can be obtained in a similar manner to mandelbrot fractals,
     by keeping 'c' constant and letting 'z' be a value of the complex plain.
 
     :param z:
     :param c:
+    :param max_iter:
     :return n: number of iterations
     """
     n = 0
-    while abs(z) <=2 and n < max_iter:
+    while abs(z) <= 2 and n < max_iter:
         z = z*z + c
         n += 1
     return n
+
+
+if __name__ == "__main__":
+
+    max_iter_ = 300
+    c_ = complex(-0.7269, 0.1889)
+
+    # Set output image resolution.
+    img_width = 1920
+    img_height = 1080
+
+    # plotting window
+    x_leftmost, x_rightmost = -2, 2
+    y_bottom, y_top = -1, 1
+    plain = np.zeros((img_width, img_height))
+
+    # For every pixel:
+    #    - convert coordinates to complex number
+    #    - assign a color w.r.t. the number of iterations
+    #      returned by the 'mandelbrot_fractal' function.
+    #    - draw color to map
+    for x in range(img_width):
+        for y in range(img_height):
+            z_ = complex(x_leftmost + (x / img_width) * (x_rightmost - x_leftmost),
+                         y_bottom + (y / img_height) * (y_top - y_bottom))
+            iters = julia_set(z_, c_, max_iter_)
+            color = iters / max_iter_
+            plain[x, y] = color
+
+    # Save the generated image.
+    plain = np.rot90(plain)
+    plt.imshow(plain, cmap='plasma', interpolation='nearest')
+    plt.axis('off')
+    plt.savefig("output/julia_c_=_-0.7269_i0.1889.png", bbox_inches='tight', pad_inches=0)
+    plt.show()
